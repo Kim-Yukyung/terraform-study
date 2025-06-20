be3f3ca3e3bf4d78944019e45554b9636878cd49
Kim-Yukyung <yukyung116@gmail.com>
2025-06-20 20:42:07 +0900

    ✨ Terraform block 작성

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.3"
+    }
+  }
+}
+
 resource "local_file" "HelloWorld" {
   content  = "Hello, World!"
   filename = "${path.module}/HelloWorld.txt"
